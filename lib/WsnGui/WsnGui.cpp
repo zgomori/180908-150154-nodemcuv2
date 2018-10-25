4491772f4c20d16e616cf354a724468ca62cb994
zgomori <gomorizo@gmail.com>
2018-10-25 09:15:00 +0200

    -

--- lib/WsnGui/WsnGui.cpp
+++ lib/WsnGui/WsnGui.cpp
@@ -10,10 +10,10 @@ WsnGui::WsnGui(TFT_eSPI *_tft){
 }
 
 void WsnGui::drawBackground(){
-	tft->fillRect(0,  	0, 	240, 24, COLOR1);
-	tft->fillRect(0,		24,	240,100, TFT_BLACK);	
-	tft->fillRect(0,  	124, 	120, 98, COLOR1);
-	tft->fillRect(120,  	124, 	120, 98, COLOR2);
-	tft->fillRect(0,  	222, 	120, 98, COLOR2);
-	tft->fillRect(120,  	222, 	120, 98, COLOR1);
+	tft->fillRect(0,		0,		240,	24,	COLOR1);
+	tft->fillRect(0,		24,	240,	100,	TFT_BLACK);	
+	tft->fillRect(0,		124,	120,	98,	COLOR1);
+	tft->fillRect(120,	124,	120,	98,	COLOR2);
+	tft->fillRect(0,		222,	120,	98,	COLOR2);
+	tft->fillRect(120,	222,	120,	98,	COLOR1);
 }
